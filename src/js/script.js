5fa8e14b27be4da9138b0703ce49abbdb1be77df
kate <kate@MacBook-Air-denisweb.local>
2020-04-30 23:49:24 +0300

    последние изменения

--- src/js/script.js
+++ src/js/script.js
@@ -408,5 +408,31 @@ then close all select boxes:*/
 document.addEventListener("click", closeAllSelect);
 
 
+// маска для телефона
+
+var phoneInput = document.querySelector('.o-form__input-phone')
+phoneInput.addEventListener('keydown', function(event) {
+   if( !(event.key == 'ArrowLeft' || event.key == 'ArrowRight' || event.key == 'Backspace' || event.key == 'Tab')) { event.preventDefault() }
+    var mask = '+7 (111) 111-11-11';
+ 
+    if (/[0-9\+\ \-\(\)]/.test(event.key)) {
+        var currentString = this.value;
+        var currentLength = currentString.length;
+        if (/[0-9]/.test(event.key)) {
+            if (mask[currentLength] == '1') {
+                this.value = currentString + event.key;
+            } else {
+                for (var i=currentLength; i<mask.length; i++) {
+                if (mask[i] == '1') {
+                    this.value = currentString + event.key;
+                    break;
+                }
+                currentString += mask[i];
+                }
+            }
+        }
+    } 
+});
+
 
 
